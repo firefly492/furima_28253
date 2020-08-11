f24c9f4dbe101bade50994b41953d4db40a141c8
firefly492 <nok.tsujimura@gmail.com>
2020-08-11 15:25:44 +0900

    出品商品の一覧表示の実装

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2020_08_08_051057) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "image", null: false
     t.string "name", null: false
     t.text "info", null: false
     t.integer "category_id", null: false
