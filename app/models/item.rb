3b81a2447190113eda8d406197e0a060a290f629
firefly492 <nok.tsujimura@gmail.com>
2020-08-14 16:05:19 +0900

    Rubocop　完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
     validates :name,                   length: { maximum: 40 }
     validates :info,                   length: { maximum: 1000 }
   end
-  
+
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
     validates :sales_status_id
@@ -24,11 +24,11 @@ class Item < ApplicationRecord
     validates :scheduled_delivery_id
   end
 
-  validates :price,                  numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   def self.save_from_yml!(yml_path)
     yml = yml.load(yml_path)
-    item = self.new
+    item = new
     item.image = yml['image']
     item.name = yml['name']
     item.info = yml['info']
@@ -41,17 +41,17 @@ class Item < ApplicationRecord
     item.save_from_yml!
   end
 
-  def self.edit_item!(image, name, info, category_id, sales_status_id, shipping_fee_status_id,prefecture_id, scheduled_delivery_id, price)
-    item = self.new
-    item.image = self.find_by!(data: image)
-    item.name = self.find_by!(data: name)
-    item.info = self.find_by!(data: info)
-    item.category_id = self.find_by!(data: category_id)
-    item.sales_status_id = self.find_by!(data: sales_status_id)
-    item.shipping_fee_status_id = self.find_by!(data: shipping_fee_status_id)
-    item.prefecture_id = self.find_by!(data: prefecture_id)
-    item.scheduled_delivery_id = self.find_by!(data: scheduled_delivery_id)
-    item.price = self.find_by!(data: price)
+  def self.edit_item!(image, name, info, category_id, sales_status_id, shipping_fee_status_id, prefecture_id, scheduled_delivery_id, price)
+    item = new
+    item.image = find_by!(data: image)
+    item.name = find_by!(data: name)
+    item.info = find_by!(data: info)
+    item.category_id = find_by!(data: category_id)
+    item.sales_status_id = find_by!(data: sales_status_id)
+    item.shipping_fee_status_id = find_by!(data: shipping_fee_status_id)
+    item.prefecture_id = find_by!(data: prefecture_id)
+    item.scheduled_delivery_id = find_by!(data: scheduled_delivery_id)
+    item.price = find_by!(data: price)
     item.save!
   end
 end
