2b594437ec6dc33d21e45fc0c16ecada1268cc9a
firefly492 <nok.tsujimura@gmail.com>
2020-08-17 17:01:30 +0900

    郵便番号・電話番号のバリデーション追加

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,10 +2,10 @@ class Transaction < ApplicationRecord
   belongs_to :item
 
   with_options presence: true do
-    validates :postal_code
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :city
     validates :addresses
-    validates :phone_number, format: { with:  /\A\d{10}$|^\d{11}\z/ } 
+    validates :phone_number, format: { with:  /\A\d{10}$|^\d{11}\z/, message: "is invalid. 11 characters."} 
   end
 
   validates :prefecture, numericality: { other_than: 1, message: "can't be blank" }
