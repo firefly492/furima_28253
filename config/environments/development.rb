3ff922c9c727c2919ee9ce94d378a7653c06d140
firefly492 <nok.tsujimura@gmail.com>
2020-08-29 15:55:05 +0900

    記述ミス修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  server '3.115.209.14', user: 'ec2-user', roles: %w{app db web}
 end
