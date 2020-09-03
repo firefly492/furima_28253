37985a0b5c4d55d66d30b7049130aeef77bd1863
firefly492 <nok.tsujimura@gmail.com>
2020-09-03 15:46:09 +0900

    item_model_l修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :address
   has_one :buyer
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   belongs_to_active_hash :category
   belongs_to_active_hash :sales_status
